64d51cfc5e6ea7dbd99815ec0e7666817a998b63
Nidigashan Kanagendran <123741227+NidiMeliodas@users.noreply.github.com>
2025-05-10 15:53:09 +0200

    coinsss

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed = 5f;
+	private Rigidbody rb;
+	private int score = 0;  // NEW: Track score
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		if (rb == null)
+		{
+			Debug.LogError("PlayerController requires a Rigidbody component.");
+		}
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+	}
+
+	// NEW: Detect trigger with coins
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Pickup"))
+		{
+			score++;
+			Debug.Log("Score: " + score);
+			other.gameObject.SetActive(false); // or use Destroy(other.gameObject);
+		}
+	}
+}
